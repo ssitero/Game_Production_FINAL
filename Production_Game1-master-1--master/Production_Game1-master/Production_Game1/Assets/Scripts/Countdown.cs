090870a8f8ab2e430ea313d6fa68606cd90d8185
Samuel <ssitero@knights.ucf.edu>
2019-02-15 14:58:55 -0500

    timer done, last 2 bugs to fix

---
+++ Production_Game1-master-1--master/Production_Game1-master/Production_Game1/Assets/Scripts/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+    [SerializeField] Text countdownText;
+
+	// Use this for initialization
+	void Start () {
+        currentTime = startingTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0){
+
+            LoseMenu.GameIsLost = true;
+            currentTime = 0;
+        }
+	}
+}
