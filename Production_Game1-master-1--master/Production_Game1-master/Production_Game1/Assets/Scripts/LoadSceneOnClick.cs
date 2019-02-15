a2da6c659ca086a854bd59bafee1b4dbf7823c84
Samuel <ssitero@knights.ucf.edu>
2019-02-15 13:59:48 -0500

    bugsbugsbugs

--- Production_Game1-master-1--master/Production_Game1-master/Production_Game1/Assets/Scripts/LoadSceneOnClick.cs
+++ Production_Game1-master-1--master/Production_Game1-master/Production_Game1/Assets/Scripts/LoadSceneOnClick.cs
@@ -8,8 +8,10 @@ public class LoadSceneOnClick : MonoBehaviour {
     public void LoadByIndex(int sceneIndex)
     {
 
-        SceneManager.LoadScene(sceneIndex);
+       // SceneManager.LoadScene(sceneIndex);
 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                                                               
     }
     public void ExitGame()
     {
